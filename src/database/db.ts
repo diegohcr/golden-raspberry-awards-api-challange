dd337be74990fdac9be140732df61bdaf3a3f9ce
diegohcr <diego_hcr@hotmail.com>
2025-02-01 14:49:29 -0300

    add Movie model and initialize in-memory SQLite database

---
+++ src/database/db.ts
@@ -0,0 +1,18 @@
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(() => {
+  db.run(`CREATE TABLE movies (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      year INTEGER,
+      title TEXT,
+      studios TEXT,
+      producers TEXT,
+      winner INTEGER
+    );`);
+
+  console.log('Database initialized');
+});
+
+export default db;
