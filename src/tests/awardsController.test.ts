0fdc2752a383ab1650e00633dca4134a0a70a071
diegohcr <diego_hcr@hotmail.com>
2025-02-01 21:33:32 -0300

    refactor awardsController tests for improved clarity and update movie data for accuracy

--- src/tests/awardsController.test.ts
+++ src/tests/awardsController.test.ts
@@ -5,75 +5,83 @@ import { db } from '../database/db';
 let server: any;
 
 beforeAll(async () => {
-  console.log('beforeAll');
   server = await startServer();
 });
 
 afterAll(async () => {
-  console.log('afterAll');
   await server.close();
+  db.close();
 });
 
 beforeEach(async () => {
-  console.log('beforeEach1');
-  //   await Promise.all([db.run('DELETE FROM movies')]);
-  await new Promise((resolve, reject) => {
+  await new Promise((resolve) => {
     db.serialize(() => {
       db.run('DELETE FROM movies');
       db.run(
         `INSERT INTO movies (year, title, studios, producers, winner) VALUES
-        (2000, 'Movie A', 'Studio A', 'Producer A', 1),
-        (2001, 'Movie B', 'Studio A', 'Producer A', 1),
-        (2010, 'Movie C', 'Studio B', 'Producer B', 1),
-        (2020, 'Movie D', 'Studio C', 'Producer B', 1)`,
+        (2000, 'Movie A', 'Studio A', 'Producer A and Producer D', 1),
+        (2001, 'Movie B', 'Studio A', 'Producer A, Producer D and Producer E', 1),
+        (2005, 'Movie C', 'Studio B', 'Producer C', 1),
+        (2010, 'Movie D', 'Studio C', 'Producer B, Producer D and Producer E', 1),
+        (2015, 'Movie E', 'Studio C', 'Producer B, Producer D and Producer E', 1),
+        (2020, 'Movie F', 'Studio C', 'Producer B, Producer D and Producer E', 1),
+        (2025, 'Movie G', 'Studio B', 'Producer C', 1),
+        (2000, 'Movie H', 'Studio D', 'Producer F', 1),
+        (2001, 'Movie I', 'Studio D', 'Producer G', 1),
+        (2002, 'Movie J', 'Studio D', 'Producer G', 1),
+        (2000, 'Movie K', 'Studio E', 'Producer H', 1),
+        (2002, 'Movie L', 'Studio E', 'Producer H', 1),
+        (2004, 'Movie M', 'Studio E', 'Producer H', 1),
+        (2024, 'Movie N', 'Studio E', 'Producer H', 1)`,
         resolve,
       );
-      const data = db.all('SELECT * FROM movies', (error, rows) => {
-        if (error != null) {
-          reject(error);
-        } else {
-          console.log('rows', rows);
-          resolve(rows);
-        }
-      });
     });
-
-    // db.run('DELETE FROM movies');
-    // db.run(
-    //   `INSERT INTO movies (year, title, studios, producers, winner) VALUES
-    //     (2000, 'Movie A', 'Studio A', 'Producer A', 'yes'),
-    //     (2001, 'Movie B', 'Studio A', 'Producer A', 'yes'),
-    //     (2010, 'Movie C', 'Studio B', 'Producer B', 'yes'),
-    //     (2020, 'Movie D', 'Studio C', 'Producer B', 'yes')`,
-    // );
-    // resolve(true);
   });
-  console.log('beforeEach2');
 });
 
 describe('GET /api/awards/intervals', () => {
-  console.log('GET /api/awards/intervals');
   it('should return corrects producer intervals', async () => {
-    console.log('GET /api/awards/intervals1');
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     const response = await request(app).get('/api/awards/intervals');
 
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('min');
     expect(response.body).toHaveProperty('max');
 
-    expect(response.body.min).toContainEqual({
-      producers: 'Producer A',
-      interval: 1,
-      previousWin: 2000,
-      followingWin: 2001,
-    });
+    expect(response.body.min).toStrictEqual([
+      {
+        producers: 'Producer A',
+        interval: 1,
+        previousWin: 2000,
+        followingWin: 2001,
+      },
+      {
+        producers: 'Producer D',
+        interval: 1,
+        previousWin: 2000,
+        followingWin: 2001,
+      },
+      {
+        producers: 'Producer G',
+        interval: 1,
+        previousWin: 2001,
+        followingWin: 2002,
+      },
+    ]);
 
-    expect(response.body.max).toContainEqual({
-      producers: 'Producer B',
-      interval: 10,
-      previousWin: 2010,
-      followingWin: 2020,
-    });
+    expect(response.body.max).toStrictEqual([
+      {
+        producers: 'Producer C',
+        interval: 20,
+        previousWin: 2005,
+        followingWin: 2025,
+      },
+      {
+        producers: 'Producer H',
+        interval: 20,
+        previousWin: 2004,
+        followingWin: 2024,
+      },
+    ]);
   });
-  console.log('GET /api/awards/intervals2');
 });
